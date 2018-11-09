f3e384d31a334b2ec2bfe2bdc936c72537108df5
Brett <>
2018-11-09 21:37:39 +0900

    Contentfulの組み込み

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,77 @@
-const pkg = require('./package')
+const contentful = require("contentful");
+const config = require("./.contentful.json");
+
+const client = contentful.createClient({
+  space: config.CTF_SPACE_ID,
+  accessToken: config.CTF_CDA_ACCESS_TOKEN
+});
+
+const pkg = require("./package");
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#FFFFFF' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#FFFFFF" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
+   ** Nuxt.js modules
+   */
+  modules: [],
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  },
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_ID: config.CTF_BLOG_ID
+  },
+  generate: {
+    routes() {
+      return Promise.all([
+        client.getEntries({
+          content_type: config.CTF_BLOG_ID
+        })
+        // client.getEntries({
+        //   content_type: config.CTF_CATEGORY_ID,
+        // }),
+      ]).then(([posts, categories]) => [
+        ...posts.items.map(post => `articles/${post.fields.id}`)
+        // ...categories.items.map(category => `articles/category/${category.fields.slug}`),
+      ]);
     }
   }
-}
+};
